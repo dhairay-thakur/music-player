46214a1ff3f03ee7fdb69b3874d659578f80f20a
dhairay-thakur <56518563+dhairay-thakur@users.noreply.github.com>
2021-03-26 13:17:41 +0530

    add play/pause song functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from "react";
-
-import "./styles/app.scss";
-
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
+import data from "./data";
+import "./styles/app.scss";
 
-function App() {
+const App = () => {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
-}
+};
 
 export default App;
